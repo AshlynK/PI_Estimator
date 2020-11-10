83323bf7380aad377f82462ee93a049f2013ee4e
ashlynjamrog <31996492+ashlynjamrog@users.noreply.github.com>
2020-11-10 14:06:38 -0800

    adding what I have

--- sub.sh
+++ sub.sh
@@ -3,13 +3,13 @@
 
 gcc -o pi main.c -w -lm -fopenmp
 
-./411/4PA/PI_Estimator/pi 1000000000 1 >> result_speedup.txt
-./411/4PA/PI_Estimator/pi  1000000000 2 >> result_speedup.txt
-./411/4PA/PI_Estimator/pi 1000000000 4 >> result_speedup.txt
-./411/4PA/PI_Estimator/pi 1000000000 8 >> result_speedup.txt
+./411/4PA/PI_Estimator/pi 1000000000 1 
+./411/4PA/PI_Estimator/pi  1000000000 2 
+./411/4PA/PI_Estimator/pi 1000000000 4 
+./411/4PA/PI_Estimator/pi 1000000000 8
 
-./411/4PA/PI_Estimator/pi 100000000 1 >> result_precision.txt
-./411/4PA/PI_Estimator/pi 200000000 2 >> result_precision.txt
-./411/4PA/PI_Estimator/pi 400000000 4 >> result_precision.txt
-./411/4PA/PI_Estimator/pi 800000000 8 >> result_precision.txt
-./411/4PA/PI_Estimator/pi 1000000000 10 >> result_precision.txt
+./411/4PA/PI_Estimator/pi 100000000 1
+./411/4PA/PI_Estimator/pi 200000000 2 
+./411/4PA/PI_Estimator/pi 400000000 4
+./411/4PA/PI_Estimator/pi 800000000 8
+./411/4PA/PI_Estimator/pi 1000000000 10
