24c20c741d9ced9a682eaa7211571a99b8ee5703
ashlynjamrog <31996492+ashlynjamrog@users.noreply.github.com>
2020-11-10 13:59:16 -0800

    new shell

--- sub.sh
+++ sub.sh
@@ -1,19 +1,15 @@
-#!/bin/bash
-
-echo "running with n=2^30, p=(1,2,4,8)"
-./411/4PA/PI_Estimator/pi 1048576 1
-./411/4PA/PI_Estimator/pi 1048576 2
-./411/4PA/PI_Estimator/pi 1048576 4
-./411/4PA/PI_Estimator/pi 1048576 8
+#!/bin/sh
 
 
-echo "running with constant n/p"
-#./411/4PA/pi 1024 8
-#./411/4PA/pi 2048 8
-#./411/4PA/pi 4096 8
-#./411/4PA/pi 8192 8
-#./411/4PA/pi 16384 8
-#./411/4PA/pi 32768 8
-#./411/4PA/pi 65536 8
-#./411/4PA/pi 131072 8
-#./411/4PA/pi 262144 8
+gcc -o pa4 pa4.c -w -lm -fopenmp
+
+./pa4 1000000000 1 >> result_speedup.txt
+./pa4 1000000000 2 >> result_speedup.txt
+./pa4 1000000000 4 >> result_speedup.txt
+./pa4 1000000000 8 >> result_speedup.txt
+
+./pa4 100000000 1 >> result_precision.txt
+./pa4 200000000 2 >> result_precision.txt
+./pa4 400000000 4 >> result_precision.txt
+./pa4 800000000 8 >> result_precision.txt
+./pa4 1000000000 10 >> result_precision.txt
