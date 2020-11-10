0537f0e98db1b6aba931cb03e43f7731191d4be1
ashlynjamrog <31996492+ashlynjamrog@users.noreply.github.com>
2020-11-10 13:55:11 -0800

    a

--- sub.sh
+++ sub.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "running with n=2^30, p=(1,2,4,8)"
-./411/4PA/pi 1048576 1
-./411/4PA/pi 1048576 2
-./411/4PA/pi 1048576 4
-./411/4PA/pi 1048576 8
+./411/4PA/PI_Estimator/pi 1048576 1
+./411/4PA/PI_Estimator/pi 1048576 2
+./411/4PA/PI_Estimator/pi 1048576 4
+./411/4PA/PI_Estimator/pi 1048576 8
 
 
 echo "running with constant n/p"
